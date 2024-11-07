8559ca472f7cbb3104bd4c3274e922aba9df98aa
JutiJayme <justinjayma1@gmail.com>
2024-11-08 06:51:26 +0800

    Add files via upload

---
+++ APP2.tsx
@@ -0,0 +1,9 @@
+import QRCodeScannerApp from '@/components/QRCodeScannerApp'
+
+export default function Home() {
+  return (
+    <main className="container py-8">
+      <QRCodeScannerApp />
+    </main>
+  )
+} 
